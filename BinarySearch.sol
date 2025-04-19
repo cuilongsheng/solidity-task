f1a27c4396d5906524eabddda6a1a54114f8128f
cuilongsheng <cuilongsheng@163.com>
2025-04-19 21:36:51 +0800

    fix: add library

--- BinarySearch.sol
+++ BinarySearch.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
+import "./LibSafeMath.sol";
+
 contract BinarySearch {
+    using SafeMath for uint[];
     // 有序数组
     // left, right 找到middleIndex,把目标值和middle比较
     function search(int256[] memory arr, int256 target)
@@ -10,7 +13,7 @@ contract BinarySearch {
         returns (int256)
     {
         // 先排序
-        int256[] memory array = bubbleSort(arr);
+        int256[] memory array = SafeMath.bubbleSort(arr);
         uint256 left = 0;
         uint256 right = array.length - 1;
         uint256 middle;
@@ -25,27 +28,5 @@ contract BinarySearch {
             }
         }
         return -1;
-    }
-
-    // 给数组排序
-    function bubbleSort(int256[] memory array)
-        public
-        pure
-        returns (int256[] memory)
-    {
-        int256[] memory arr = array;
-        uint256 n = arr.length;
-        for (uint256 i = 0; i < n - 1; i++) {
-            bool swapped = false;
-            for (uint256 j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    // 交换 arr[j] 和 arr[j+1]
-                    (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
-                    swapped = true;
-                }
-            }
-            if (!swapped) break;
-        }
-        return arr;
     }
 }
